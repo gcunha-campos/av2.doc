5400bebb556ba41b6e5eddc6d746f1c8ab20acff
GABRIEL CUNHA CAMPOS DIEGUEZ <101563389+gcunha-campos@users.noreply.github.com>
2023-06-28 17:22:05 -0300

    Add files via upload
    OMMIT FINAL

--- menudata.js
+++ menudata.js
@@ -26,6 +26,6 @@ var menudata={children:[
 {text:"Página Principal",url:"index.html"},
 {text:"Arquivos",url:"files.html",children:[
 {text:"Lista de Arquivos",url:"files.html"},
-{text:"Membros dos Arquivos",url:"globals.html",children:[
+{text:"Globais",url:"globals.html",children:[
 {text:"Todos",url:"globals.html"},
 {text:"Funções",url:"globals_func.html"}]}]}]}
